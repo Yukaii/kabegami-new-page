9f5e52938a2eb7579a3e434529c9c74a898b9600
Yukai Huang <yukaihuangtw@gmail.com>
2018-08-26 19:01:32 +0800

    Add fadeout transition

--- src/app/components/NotificationCenter.vue
+++ src/app/components/NotificationCenter.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="notifications-container">
     <div class="flash"
-      :class="typeClass(not.type)"
+      :class="['notification', typeClass(not.type), { removing: isRemoving(not) }]"
       v-for="not in notifications"
+      :id="not.id"
       :key="not.id"
     >
       <button
@@ -51,7 +52,23 @@ export default class NotificationCenter extends AppProps {
     }
 
     this.notificationMap = { ...this.notificationMap, [notification.id]: notification }
-    window.setTimeout(() => this.clearNotification(notification.id), duration)
+    window.setTimeout(() => {
+      this.notificationMap = {
+        ...this.notificationMap,
+        [notification.id]: {
+          ...notification,
+          isRemoving: true
+        }
+      }
+
+      this.$el.querySelector(`.notification[id="${notification.id}"]`).addEventListener('transitionend', () => {
+        this.clearNotification(notification.id)
+      })
+    }, duration)
+  }
+
+  isRemoving (notification) {
+    return notification.isRemoving
   }
 
   clearNotification (notificationId) {
@@ -87,5 +104,12 @@ export default class NotificationCenter extends AppProps {
   min-width: 350px;
   padding-top: 1em;
   padding-right: 1em;
+  opacity: 1;
+
+  .notification.removing {
+    opacity: 0;
+    transform: translateY(-100%);
+    transition: all ease-in-out 450ms;
+  }
 }
 </style>
